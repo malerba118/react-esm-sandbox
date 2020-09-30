06bf33f42efb8e474143505eccf1528e57c66bc7
Austin Malerba <austin.malerba@gmail.com>
2020-09-29 20:41:25 -0500

    stuff

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,43 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Playground, SkypackImportMap } from 'react-esm-sandbox'
-import { BabelTypescriptTransform } from 'react-esm-sandbox/transforms'
+import {
+  Playground,
+  SkypackImportMap,
+  SourceFile,
+  BabelTypescriptTransform
+} from 'react-esm-sandbox'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/material.css'
-
-const files = [
-  {
-    path: 'index.tsx',
-    contents: `
-      import React from 'react';
-      import ReactDOM from 'react-dom';
-      import { Typography } from '@material-ui/core';
-      import { add, subtract } from './math/index.js';
-
-      ReactDOM.render(<Typography>{add(50, subtract(20, 10))}</Typography>, document.body)
-    `
-  },
-  {
-    path: 'math/add.js',
-    contents: `
-      export const add = (a, b) => a + b;
-    `
-  },
-  {
-    path: 'math/subtract.js',
-    contents: `
-      export const subtract = (a, b) => a - b;
-    `
-  },
-  {
-    path: 'math/index.js',
-    contents: `
-      export * from './add.js'
-      export * from './subtract.js'
-    `
-  }
-]
 
 const importMap = SkypackImportMap({
   react: 'latest',
@@ -51,6 +22,50 @@ const transforms = {
 }
 
 const App = () => {
+  const [files, setFiles] = useState([
+    {
+      path: 'index.tsx',
+      contents: `
+        import React from 'react';
+        import ReactDOM from 'react-dom';
+        import { Typography } from '@material-ui/core';
+        import { add, subtract } from './math/index.js';
+  
+        ReactDOM.render(<Typography>{add(50, subtract(20, 10))}</Typography>, document.body)
+      `
+    },
+    {
+      path: 'math/add.js',
+      contents: `
+        export const add = (a, b) => a + b;
+      `
+    },
+    {
+      path: 'math/subtract.js',
+      contents: `
+        export const subtract = (a, b) => a - b;
+      `
+    },
+    {
+      path: 'math/index.js',
+      contents: `
+        export * from './add.js'
+        export * from './subtract.js'
+      `
+    }
+  ])
+
+  const updateFile = (file: SourceFile) => {
+    setFiles((prev) =>
+      prev.map((f) => {
+        if (f.path === file.path) {
+          return file
+        }
+        return f
+      })
+    )
+  }
+
   return (
     <Playground
       selectedFile='index.tsx'
@@ -61,6 +76,7 @@ const App = () => {
       importMap={importMap}
       onLog={(data: any) => window.alert(JSON.stringify(data))}
       transforms={transforms}
+      onChange={updateFile}
     />
   )
 }
